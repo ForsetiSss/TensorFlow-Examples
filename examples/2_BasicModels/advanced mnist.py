2003d64b1b352cf37d9dd9680b9979520091ee56
Forseti_B220 <695459603@qq.com>
2019-04-29 21:41:25 +0800

    20190429 update

---
+++ examples/2_BasicModels/advanced mnist.py
@@ -0,0 +1,30 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.InteractiveSession()
+# 更加方便的InteractiveSession类。通过它，你可以更加灵活地构建你的代码。它能让你在运行图的时候，插入一些计算图。
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+
+#训练模型
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+for i in range(1000):
+    batch = mnist.train.next_batch(50)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+# 评估模型
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
